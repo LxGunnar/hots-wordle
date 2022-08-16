72be84deb4ca010908b35c1a68bb122a239ce6d9
LxGunnar <39277473+LxGunnar@users.noreply.github.com>
2022-08-16 19:12:02 -0300

    Mudança 2
    Mudando o nome para TermoHOTS.
    Traduzindo as configurações
    Adicionando sobre o que é termohots

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,13 +8,22 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="Como Jogar TermoHOTS" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="TermoHOTS" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Adivinhe a palavra relacionada ao jogo Heroes of the Storm em 6 tentavitas. 
+        TermoHOTS é uma versão baseada em Heroes of the Storm, moba da Blizzard. 
+      </p>
+
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Adivinhe a palavra relacionada ao jogo HOTS em 6 tentavitas. 
         Após cada palpite, a cor das peças mudará para mostrar quão próximo seu palpite estava da palavra.
       </p>
+
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Modo de Jogar: Todas as palavras serão em Português. Você poderá usar palavras do vocábulario a fim de tentar acertar.
+        <strong>Modo de Jogar:</strong> Todas as palavras serão em Português. Você poderá usar palavras do vocábulario a fim de tentar acertar.
+      </p>
+
+      <p className="text-sm italic text-gray-500 dark:text-gray-300">
+        O TermoHOTS não se limita apenas a 5 letras.
       </p>
 
       <div className="mb-1 mt-4 flex justify-center">
@@ -55,7 +64,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
 
       <div className="mb-1 mt-4 flex justify-center">
         <Cell value="B" />
-        <Cell value="A" />
+        <Cell value="Á" />
         <Cell value="S" />
         <Cell isRevealing={true} isCompleted={true} value="I" status="absent" />
         <Cell value="C" />
@@ -65,15 +74,33 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         A letra I não está na palavra em nenhum lugar.
       </p>
 
-      <p className="mt-6 text-sm italic text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+      <p className="p-3">
+      <hr></hr>
+      </p>
+
+      <h2 className="text-center text-lg leading-6 font-medium text-gray-900 dark:text-white" id="headlessui-dialog-title-7">Sobre</h2>
+
+      <p className="mt-6 text-sm text-gray-500 dark:text-gray-300">
+         Baseado no código de
         <a
           href="https://github.com/cwackerfuss/react-wordle"
-          className="font-bold underline"
-        >
-          check out the code here
-        </a>{' '}
+          className="font-bold"
+          target="_blank"
+          rel="nofollow"
+          > Cwackerfuss</a>
+      </p>
+
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+         Adaptado por Gustavo "Gunnar"
+      </p>
+
+        <p className="mt-6 text-sm text-gray-500 dark:text-gray-300">
+        <a
+          href="https://linktr.ee/CLMTgaming"
+          className="font-bold"
+          target="_blank"
+          rel="nofollow"
+          >Siga a Calamity Gaming</a>.
       </p>
     </BaseModal>
   )
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -5,16 +5,16 @@ export const GAME_COPIED_MESSAGE = 'Jogo copiado para a área de transferência'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Letras insuficientes'
 export const WORD_NOT_FOUND_MESSAGE = 'Palavra não encontrada'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode só pode ser ativado no início!'
+  'Modo Difícil só pode ser ativado no início!'
 export const HARD_MODE_DESCRIPTION =
-  'Any revealed hints must be used in subsequent guesses'
+  'A letra revelada deve ser utilizada na mesma posição suposições posteriores.'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Para melhor visão de cores'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
   `A palavra era ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
+  `Deve usar a letra ${guess} na posição ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
+  `A palavra deve conter a letra ${letter}`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
 export const STATISTICS_TITLE = 'Estatísticas'
@@ -22,7 +22,7 @@ export const GUESS_DISTRIBUTION_TEXT = 'Distribuição das Partidas'
 export const NEW_WORD_TEXT = 'Nova palavra em:'
 export const SHARE_TEXT = 'Compartilhar'
 export const SHARE_FAILURE_TEXT =
-  'Unable to share the results. This feature is available only in secure contexts (HTTPS), in some or all supporting browsers.'
+  'Incapaz de compartilhar os resultados. Esta característica está disponível apenas em contextos seguros (HTTPS), em alguns ou todos os navegadores de suporte.'
 export const MIGRATE_BUTTON_TEXT = 'Transferir'
 export const MIGRATE_DESCRIPTION_TEXT =
   'Clique aqui para transferir suas estatísticas para um novo dispositivo.' 
@@ -31,4 +31,4 @@ export const SUCCESS_RATE_TEXT = 'Taxa de acerto'
 export const CURRENT_STREAK_TEXT = 'Sequência Atual'
 export const BEST_STREAK_TEXT = 'Melhor Sequência'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
-  "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
+  "Você está usando um navegador incorporado e pode ter problemas para compartilhar ou salvar seus resultados. Nós o encorajamos a usar o navegador padrão de seu dispositivo."
